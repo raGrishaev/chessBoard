c360577c9ebb4d828d0f09e0ae80ba34d1a2898b
raGrishaev <ro.grishaev@gmail.com>
2022-01-10 21:30:31 +0300

    firstClass

--- src/start.java
+++ src/start.java
@@ -1,2 +1,17 @@
 public class start {
+    public static void main (String [] args) {
+        System.out.println(fibonacciNumber(10));
+    }
+    public static int fibonacciNumber(int degree){
+        int firstNumber = 1;
+        int secondNumber = 1;
+        int thirdNumber = 1;
+
+        for(int i = 3; i <= degree; i++){
+            thirdNumber = secondNumber + firstNumber;
+            firstNumber = secondNumber;
+            secondNumber = thirdNumber;
+        }
+        return thirdNumber;
+    }
 }
